6fc23379d2cb98887d0d8388dd1d073736d5043d
xvlika <shengeliagiorgi114@gmail.com>
2024-06-15 16:02:49 -0700

    amis deda vatire araperi ar mushaobs wesierad bliad

--- countries-prediction/countries_info.py
+++ countries-prediction/countries_info.py
@@ -1,169 +1,184 @@
 import shelve
 
-with shelve.open("countries") as db:
-    db['countries_gdp'] = [
-        {
-            "country": "United States",
-            "gdp": 25.43, 
-            "unemployment": 4.0,
-            "inflation": 2.1,
-            "tax_percentage": 27.1,
-            "trade_balance": -621,
-            "investment_rate": 18.6,
-            "population_growth": 0.7,
-            "exchange_rate": 1.0
-        },
-        {
-            "country": "China",
-            "gdp": 14.72,
-            "unemployment": 5.3,
-            "inflation": 2.5,
-            "tax_percentage": 17.1,
-            "trade_balance": 370, 
-            "investment_rate": 43.3,
-            "population_growth": 0.3,
-            "exchange_rate": 6.4
-        },
-        {
-            "country": "Japan",
-            "gdp": 4.25,
-            "unemployment": 2.6,
-            "inflation": 0.5, 
-            "tax_percentage": 30.6,
-            "trade_balance": 15,
-            "investment_rate": 23.4,
-            "population_growth": -0.2,
-            "exchange_rate": 110.0
-        },
-        {
-            "country": "Germany",
-            "gdp": 3.85,
-            "unemployment": 3.2,
-            "inflation": 1.7,
-            "tax_percentage": 38.2,
-            "trade_balance": 210,
-            "investment_rate": 20.1,
-            "population_growth": 0.2,
-            "exchange_rate": 0.9
-        },
-        {
-            "country": "India", 
-            "gdp": 3.41,
-            "unemployment": 7.0,
-            "inflation": 3.9,
-            "tax_percentage": 17.7, 
-            "trade_balance": -101,
-            "investment_rate": 30.1,
-            "population_growth": 1.2,
-            "exchange_rate": 74.0
-        },
-        {
-            "country": "United Kingdom",
-            "gdp": 2.67,
-            "unemployment": 4.8,
-            "inflation": 1.5,
-            "tax_percentage": 33.3,
-            "trade_balance": -125,
-            "investment_rate": 17.1,
-            "population_growth": 0.6, 
-            "exchange_rate": 0.75
-        },
-        {
-            "country": "France", 
-            "gdp": 2.63,
-            "unemployment": 8.5,
-            "inflation": 1.3, 
-            "tax_percentage": 45.4,
-            "trade_balance": -74,
-            "investment_rate": 23.0,
-            "population_growth": 0.2,
-            "exchange_rate": 0.9
-        },
-        {
-            "country": "Russia",
-            "gdp": 2.24,
-            "unemployment": 6.1,
-            "inflation": 3.4, 
-            "tax_percentage": 22.2,
-            "trade_balance": 162,
-            "investment_rate": 24.6,
-            "population_growth": -0.1,
-            "exchange_rate": 72.0
-        },
-        {
-            "country": "Canada",
-            "gdp": 2.16, 
-            "unemployment": 5.8,
-            "inflation": 2.0, 
-            "tax_percentage": 31.2,
-            "trade_balance": -13,
-            "investment_rate": 22.5,
-            "population_growth": 1.1,
-            "exchange_rate": 1.3
-        },
-        {
-            "country": "Italy",
-            "gdp": 2.04,
-            "unemployment": 9.1,
-            "inflation": 0.9, 
-            "tax_percentage": 42.4, 
-            "trade_balance": 54,
-            "investment_rate": 17.5,
-            "population_growth": -0.2,
-            "exchange_rate": 0.9
-        },
-        {
-            "country": "Brazil",
-            "gdp": 1.92,
-            "unemployment": 12.0,
-            "inflation": 3.7,
-            "tax_percentage": 33.1,
-            "trade_balance": 48,
-            "investment_rate": 15.4,
-            "population_growth": 0.8,
-            "exchange_rate": 5.2
-        },
-        {
-            "country": "Australia",
-            "gdp": 1.69,
-            "unemployment": 4.2,
-            "inflation": 1.8,
-            "tax_percentage": 27.8,
-            "trade_balance": 25,
-            "investment_rate": 26.2,
-            "population_growth": 1.3,
-            "exchange_rate": 1.4
-        },
-        {
-            "country": "South Korea",
-            "gdp": 1.67, 
-            "unemployment": 3.8,
-            "inflation": 1.1, 
-            "tax_percentage": 24.1, 
-            "trade_balance": 70,
-            "investment_rate": 29.1,
-            "population_growth": 0.3,
-            "exchange_rate": 1120.0
-        },
-        {
-            "country": "Mexico", 
-            "gdp": 1.46,
-            "unemployment": 4.0,
-            "inflation": 4.1,
-            "tax_percentage": 16.5,
-            "trade_balance": -14,
-            "investment_rate": 21.9,
-            "population_growth": 1.0, "exchange_rate": 20.0
-        },
-        {
-            "country": "Spain",
-            "gdp": 1.41,
-            "unemployment": 14.0,
-            "inflation": 0.6, 
-            "tax_percentage": 35.4,
-            "trade_balance": -35,
-            "investment_rate": 21.2,
-            "population_growth": 0.0,
-            "exchange_rate": 0.9
-        }
-    ]
+countries_data = [
+    {
+        "country": "United States",
+        "gdp": 25.43,
+        "gdp_growth_rate": 2.2,  
+        "unemployment": 4.0,
+        "inflation": 2.1,
+        "tax_percentage": 27.1,
+        "trade_balance": -621,
+        "investment_rate": 18.6,
+        "population_growth": 0.7,
+        "exchange_rate": 1.0
+    },
+    {
+        "country": "China",
+        "gdp": 14.72,
+        "gdp_growth_rate": 6.5,
+        "unemployment": 5.3,
+        "inflation": 2.5,
+        "tax_percentage": 17.1,
+        "trade_balance": 370,
+        "investment_rate": 43.3,
+        "population_growth": 0.3,
+        "exchange_rate": 6.4
+    },
+    {
+        "country": "Japan",
+        "gdp": 4.25,
+        "gdp_growth_rate": 0.8,
+        "unemployment": 2.6,
+        "inflation": 0.5,
+        "tax_percentage": 30.6,
+        "trade_balance": 15,
+        "investment_rate": 23.4,
+        "population_growth": -0.2,
+        "exchange_rate": 110.0
+    },
+    {
+        "country": "Germany",
+        "gdp": 3.85,
+        "gdp_growth_rate": 1.5,
+        "unemployment": 3.2,
+        "inflation": 1.7,
+        "tax_percentage": 38.2,
+        "trade_balance": 210,
+        "investment_rate": 20.1,
+        "population_growth": 0.2,
+        "exchange_rate": 0.9
+    },
+    {
+        "country": "India",
+        "gdp": 3.41,
+        "gdp_growth_rate": 7.3,
+        "unemployment": 7.0,
+        "inflation": 3.9,
+        "tax_percentage": 17.7,
+        "trade_balance": -101,
+        "investment_rate": 30.1,
+        "population_growth": 1.2,
+        "exchange_rate": 74.0
+    },
+    {
+        "country": "United Kingdom",
+        "gdp": 2.67,
+        "gdp_growth_rate": 1.3,
+        "unemployment": 4.8,
+        "inflation": 1.5,
+        "tax_percentage": 33.3,
+        "trade_balance": -125,
+        "investment_rate": 17.1,
+        "population_growth": 0.6,
+        "exchange_rate": 0.75
+    },
+    {
+        "country": "France",
+        "gdp": 2.63,
+        "gdp_growth_rate": 1.4,
+        "unemployment": 8.5,
+        "inflation": 1.3,
+        "tax_percentage": 45.4,
+        "trade_balance": -74,
+        "investment_rate": 23.0,
+        "population_growth": 0.2,
+        "exchange_rate": 0.9
+    },
+    {
+        "country": "Russia",
+        "gdp": 2.24,
+        "gdp_growth_rate": 1.8,
+        "unemployment": 6.1,
+        "inflation": 3.4,
+        "tax_percentage": 22.2,
+        "trade_balance": 162,
+        "investment_rate": 24.6,
+        "population_growth": -0.1,
+        "exchange_rate": 72.0
+    },
+    {
+        "country": "Canada",
+        "gdp": 2.16,
+        "gdp_growth_rate": 1.9,
+        "unemployment": 5.8,
+        "inflation": 2.0,
+        "tax_percentage": 31.2,
+        "trade_balance": -13,
+        "investment_rate": 22.5,
+        "population_growth": 1.1,
+        "exchange_rate": 1.3
+    },
+    {
+        "country": "Italy",
+        "gdp": 2.04,
+        "gdp_growth_rate": 1.0,
+        "unemployment": 9.1,
+        "inflation": 0.9,
+        "tax_percentage": 42.4,
+        "trade_balance": 54,
+        "investment_rate": 17.5,
+        "population_growth": -0.2,
+        "exchange_rate": 0.9
+    },
+    {
+        "country": "Brazil",
+        "gdp": 1.92,
+        "gdp_growth_rate": 0.5,
+        "unemployment": 12.0,
+        "inflation": 3.7,
+        "tax_percentage": 33.1,
+        "trade_balance": 48,
+        "investment_rate": 15.4,
+        "population_growth": 0.8,
+        "exchange_rate": 5.2
+    },
+    {
+        "country": "Australia",
+        "gdp": 1.69,
+        "gdp_growth_rate": 2.5,
+        "unemployment": 4.2,
+        "inflation": 1.8,
+        "tax_percentage": 27.8,
+        "trade_balance": 25,
+        "investment_rate": 26.2,
+        "population_growth": 1.3,
+        "exchange_rate": 1.4
+    },
+    {
+        "country": "South Korea",
+        "gdp": 1.67,
+        "gdp_growth_rate": 3.0,
+        "unemployment": 3.8,
+        "inflation": 1.1,
+        "tax_percentage": 24.1,
+        "trade_balance": 70,
+        "investment_rate": 29.1,
+        "population_growth": 0.3,
+        "exchange_rate": 1120.0
+    },
+    {
+        "country": "Mexico",
+        "gdp": 1.46,
+        "gdp_growth_rate": 2.1,
+        "unemployment": 4.0,
+        "inflation": 4.1,
+        "tax_percentage": 16.5,
+        "trade_balance": -14,
+        "investment_rate": 21.9,
+        "population_growth": 1.0,
+        "exchange_rate": 20.0
+    },
+    {
+        "country": "Spain",
+        "gdp": 1.41,
+        "gdp_growth_rate": 1.6,
+        "unemployment": 14.0,
+        "inflation": 0.6,
+        "tax_percentage": 35.4,
+        "trade_balance": -35,
+        "investment_rate": 21.2,
+        "population_growth": 0.0,
+        "exchange_rate": 0.9
+    }
+]
--- countries-prediction/displayInfo.py
+++ countries-prediction/displayInfo.py
@@ -1,70 +1,120 @@
 import shelve
+import math
 
-while True:
-    userChoice = input("Enter which country info you want to see (if you want all enter: all, to quit enter: -1): ").lower()
-    
-    if userChoice == "-1":
-        print("Exiting the program.")
-        break
-    
-    with shelve.open("countries") as db:
-        countries_gdp = db['countries_gdp']
+def display_current():
+    while True:
+        userChoice = input("Enter which country info you want to see (if you want all enter: all, to quit enter: -1): ").lower()
         
-        if userChoice == "all":
-            for country_data in countries_gdp:
-                country = country_data['country']
-                gdp = country_data['gdp']
-                unemployment = country_data["unemployment"]
-                inflation = country_data["inflation"]
-                tax = country_data["tax_percentage"]
-                trade = country_data["trade_balance"]
-                investment_rate = country_data['investment_rate']
-                population_growth = country_data["population_growth"]
-                exchange_rate = country_data["exchange_rate"]
-
-                print(f"{'='*40}")
-                print(f"Country:           {country}")
-                print(f"{'-'*40}")
-                print(f"GDP:               {gdp} trillion USD")
-                print(f"Unemployment Rate: {unemployment}%")
-                print(f"Inflation Rate:    {inflation}%")
-                print(f"Taxation:          {tax}%")
-                print(f"Trade Balance:     {trade}")
-                print(f"Investment Rate:   {investment_rate}%")
-                print(f"Population Growth: {population_growth}%")
-                print(f"Exchange Rate:     {exchange_rate}")
-                print(f"{'='*40}\n")
-        else:
-            choices = userChoice.split(',')
-            for choice in choices:
-                choice = choice.strip()
-                found = False
+        if userChoice == "-1":
+            print("Exiting the program.")
+            break
+        
+        with shelve.open("countries") as db:
+            countries_gdp = db['countries_gdp']
+            
+            if userChoice == "all":
                 for country_data in countries_gdp:
-                    country = country_data['country'].lower()
-                    if choice == country:
-                        gdp = country_data['gdp']
-                        unemployment = country_data["unemployment"]
-                        inflation = country_data["inflation"]
-                        tax = country_data["tax_percentage"]
-                        trade = country_data["trade_balance"]
-                        investment_rate = country_data['investment_rate']
-                        population_growth = country_data["population_growth"]
-                        exchange_rate = country_data["exchange_rate"]
+                    country = country_data['country']
+                    gdp = country_data['gdp']
+                    gdp_growth = country_data['gdp_growth_rate']
+                    unemployment = country_data["unemployment"]
+                    inflation = country_data["inflation"]
+                    tax = country_data["tax_percentage"]
+                    trade = country_data["trade_balance"]
+                    investment_rate = country_data['investment_rate']
+                    population_growth = country_data["population_growth"]
+                    exchange_rate = country_data["exchange_rate"]
 
-                        print(f"{'='*40}")
-                        print(f"Country:           {country_data['country']}")
-                        print(f"{'-'*40}")
-                        print(f"GDP:               {gdp} trillion USD")
-                        print(f"Unemployment Rate: {unemployment}%")
-                        print(f"Inflation Rate:    {inflation}%")
-                        print(f"Taxation:          {tax}%")
-                        print(f"Trade Balance:     {trade}")
-                        print(f"Investment Rate:   {investment_rate}%")
-                        print(f"Population Growth: {population_growth}%")
-                        print(f"Exchange Rate:     {exchange_rate}")
-                        print(f"{'='*40}\n")
-                        found = True
-                        break
+                    print(f"{'='*40}")
+                    print(f"Country:           {country}")
+                    print(f"{'-'*40}")
+                    print(f"GDP:               {gdp} trillion USD")
+                    print(f"GDP Growth:        {gdp_growth_rate}%")
+                    print(f"Unemployment Rate: {unemployment}%")
+                    print(f"Inflation Rate:    {inflation}%")
+                    print(f"Taxation:          {tax}%")
+                    print(f"Trade Balance:     {trade}")
+                    print(f"Investment Rate:   {investment_rate}%")
+                    print(f"Population Growth: {population_growth}%")
+                    print(f"Exchange Rate:     {exchange_rate}")
+                    print(f"{'='*40}\n")
+            else:
+                choices = userChoice.split(',')
+                for choice in choices:
+                    choice = choice.strip()
+                    found = False
+                    for country_data in countries_gdp:
+                        country = country_data['country'].lower()
+                        if choice == country:
+                            gdp = country_data['gdp']
+                            gdp_growth = country_data['gdp_growth_rate']
+                            unemployment = country_data["unemployment"]
+                            inflation = country_data["inflation"]
+                            tax = country_data["tax_percentage"]
+                            trade = country_data["trade_balance"]
+                            investment_rate = country_data['investment_rate']
+                            population_growth = country_data["population_growth"]
+                            exchange_rate = country_data["exchange_rate"]
 
-                if not found:
-                    print(f"Information for '{choice}' is not available or the country name is incorrect.")
+                            print(f"{'='*40}")
+                            print(f"Country:           {country_data['country']}")
+                            print(f"{'-'*40}")
+                            print(f"GDP:               {gdp} trillion USD")
+                            print(f"GDP Growth:        {gdp_growth_rate}%")
+                            print(f"Unemployment Rate: {unemployment}%")
+                            print(f"Inflation Rate:    {inflation}%")
+                            print(f"Taxation:          {tax}%")
+                            print(f"Trade Balance:     {trade}")
+                            print(f"Investment Rate:   {investment_rate}%")
+                            print(f"Population Growth: {population_growth}%")
+                            print(f"Exchange Rate:     {exchange_rate}")
+                            print(f"{'='*40}\n")
+                            found = True
+                            break
+
+                    if not found:
+                        print(f"Information for '{choice}' is not available or the country name is incorrect.")
+
+def est_future_gdp(current_gdp, years , gdp_growth , inflation, population_growth, tax , trade , investment, exchange):
+    exchange_ratio = math.log()
+    future_gdp = current_gdp * (1+(gdp_growth - inflation-(tax/100)+(trade/current_gdp)+(investment/100)+(population_growth/100)/100))**years
+    return future_gdp
+
+def country_after():
+    user_input_year = int(input("Enter how many year simulation do you want: "))
+    country_name = input("Enter country/countries names: ").strip().lower()
+    with shelve.open("countries") as db:
+        countries_gdp = db('countries_gdp')
+        found = False
+        for country_data in countries_gdp:
+            country = country_data['country']
+            gdp = country_data['gdp']
+            gdp_growth = country_data['gdp_growth_rate']
+            unemployment = country_data["unemployment"]
+            inflation = country_data["inflation"]
+            tax = country_data["tax_percentage"]
+            trade = country_data["trade_balance"]
+            investment_rate = country_data['investment_rate']
+            population_growth = country_data["population_growth"]
+            exchange_rate = country_data["exchange_rate"]
+        for country_data in countries_gdp:
+            if country_data.lower() == country.lower():
+                found = True
+                future_gdp = est_future_gdp(gdp,user_input_year,gdp_growth,inflation,population_growth,tax,trade,investment_rate,exchange_rate)            
+                print(f"{country.capitalize()} gdp after {user_input_year} years is {future_gdp}")
+
+    
+def main():
+    print("Enter your choice: ")
+    print("1.Display country's current info")
+    print("2.Display country's info after X years later")
+    print("3.Manual simulation (user inputs data of country)")
+    choice = int(input("Enter Your Choice: "))
+    if choice == 1:
+        display_current()
+    elif  choice == 2: 
+        country_after()
+
+
+if __name__ == "__main__":
+    main()
